098437b9969e73fac18394d1f3dc09239766da6b
ocuser <masterpiece.015v@gmail.com>
2019-10-30 16:33:38 +0900

    default

--- app/src/main/java/com/example/ocuser/ocapp/MainActivity.kt
+++ app/src/main/java/com/example/ocuser/ocapp/MainActivity.kt
@@ -4,12 +4,7 @@ import android.media.AudioAttributes
 import android.media.SoundPool
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.constraint.ConstraintLayout
-import android.support.constraint.ConstraintSet
-import android.util.Log
-import android.view.View
 import android.widget.Button
-import android.widget.ImageView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -21,22 +16,24 @@ class MainActivity : AppCompatActivity() {
     private lateinit var soundPool : SoundPool
     val scoreList = mutableListOf<Note>()        //楽譜
     val scaleMap = mutableMapOf<String,Int>()    //音階と音源の対応
-    lateinit var btn_do1 : Button                //ドの鍵盤
-    lateinit var btn_re1 : Button                //レの鍵盤
-    lateinit var btn_mi1 : Button                //ミの鍵盤
-    lateinit var btn_fa1 : Button                //ファの鍵盤
-    lateinit var btn_so1 : Button                //ソの鍵盤
-    lateinit var btn_ra1 : Button                //ラの鍵盤
-    lateinit var btn_si1 : Button                //シの鍵盤
-    lateinit var btn_do2 : Button               //高いドの鍵盤
-    lateinit var btn_play : Button               //再生のボタン
-    lateinit var btn_clear : Button             //クリアのボタン
+    lateinit var do1 : Button                //ドの鍵盤
+    lateinit var re1 : Button                //レの鍵盤
+    lateinit var mi1 : Button                //ミの鍵盤
+    lateinit var fa1 : Button                //ファの鍵盤
+    lateinit var so1 : Button                //ソの鍵盤
+    lateinit var ra1 : Button                //ラの鍵盤
+    lateinit var si1 : Button                //シの鍵盤
+    lateinit var do2 : Button               //高いドの鍵盤
+    lateinit var play : Button               //再生のボタン
+    lateinit var clear : Button             //クリアのボタン
 
+    //アプリケーションが起動するときに自動で動く
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //初期化
         init()
+
     }
 
     //ドの鍵盤
@@ -89,7 +86,7 @@ class MainActivity : AppCompatActivity() {
     //scoreListを再生する非同期プログラム
     fun scorePlay(){
         GlobalScope.launch( Dispatchers.Main ){
-            btn_play.isEnabled = false
+            play.isEnabled = false
             async( Dispatchers.Default ){
                 scoreList.forEach{
                     if( it.oto != "休") {
@@ -99,7 +96,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 return@async
             }.await()
-            btn_play.isEnabled = true
+            play.isEnabled = true
         }
     }
 
@@ -118,41 +115,41 @@ class MainActivity : AppCompatActivity() {
         scaleMap.put("ド2",  soundPool.load(this,R.raw.s_do2,1))
 
         //鍵盤にイベントを割り当てる
-        btn_do1 = findViewById(R.id.btn_do1)
-        btn_do1.setOnClickListener{
+        do1 = findViewById(R.id.do1)
+        do1.setOnClickListener{
             btnDo1Click()
                     }
-        btn_re1 = findViewById(R.id.btn_re1)
-        btn_re1.setOnClickListener{
+        re1 = findViewById(R.id.re1)
+        re1.setOnClickListener{
             btnRe1Click()
                     }
-        btn_mi1=findViewById(R.id.btn_mi1)
-        btn_mi1.setOnClickListener{
+        mi1=findViewById(R.id.mi1)
+        mi1.setOnClickListener{
             btnMi1Click()
                    }
-        btn_fa1=findViewById(R.id.btn_fa1)
-        btn_fa1.setOnClickListener {
+        fa1=findViewById(R.id.fa1)
+        fa1.setOnClickListener {
             btnFa1Click()
         }
-        btn_so1=findViewById(R.id.btn_so1)
-        btn_so1.setOnClickListener{
+        so1=findViewById(R.id.so1)
+        so1.setOnClickListener{
             btnSo1Click()
                     }
-        btn_ra1=findViewById(R.id.btn_ra1)
-        btn_ra1.setOnClickListener {
+        ra1=findViewById(R.id.ra1)
+        ra1.setOnClickListener {
             btnRa1Click()
         }
-        btn_si1=findViewById(R.id.btn_si1)
-        btn_si1.setOnClickListener{
+        si1=findViewById(R.id.si1)
+        si1.setOnClickListener{
             btnSi1Click()
         }
-        btn_do2= findViewById( R.id.btn_do2)
-        btn_do2.setOnClickListener{
+        do2= findViewById( R.id.do2)
+        do2.setOnClickListener{
             btnDo2Click()
         }
         //楽譜を再生ボタン
-        btn_play = findViewById(R.id.btn_play)
-        btn_play.setOnClickListener{
+        play = findViewById(R.id.play)
+        play.setOnClickListener{
             makeScore()
             scorePlay()
         }
